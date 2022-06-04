f3677bc3ad402951fc87175fb7dcd837b2d0f500
Juliano Xavier Diniz <julianoxavierdinizjuju@gmail.com>
2022-06-03 23:11:54 -0300

    Update

--- ListTs.java
+++ ListTs.java
@@ -2,31 +2,23 @@ public class ListTS {
     public Object[] list;
     public int length;
 
-    public ListTS() {
-        list = new Object[0];
-    }
+    public ListTS() { list = new Object[0]; }
 
     public void add(Object value) {
         final int one = 1;
         int z = this.length += one;
-
         Object[] newL = new Object[z];
-        for (int i = 0; i < list.length; i++) {
-            newL[i] = list[i];
-        }
+        for (int i = 0; i != list.length; i++) newL[i] = list[i];
         newL[z - one] = value;
         list = newL;
     }
 
     public void remove(int index) {
         final int z = 0;
-        if (index < z || index > this.length)
-            return;
+        if (index < z || index > this.length) return;
         final int one = 1;
         Object[] newL = new Object[this.length -= one];
-        for (int i = z; i < list.length; i++) {
-            newL[i > index ? i - one : i] = list[i];
-        }
+        for (int i = z; i != list.length; i++) newL[i > index ? i - one : i] = list[i];
         list = newL;
     }
 
@@ -34,8 +26,7 @@ public class ListTS {
 
     public void revertPos(int fromHere, int toHere) {
         final int z = 0;
-        if (fromHere < z || fromHere > length || toHere < z || toHere > length || fromHere == toHere)
-            return;
+        if (fromHere < z || fromHere > length || toHere < z || toHere > length || fromHere == toHere) return;
         final Object ob0 = list[fromHere];
         final Object ob1 = list[toHere];
         list[fromHere] = ob1;
@@ -47,19 +38,14 @@ public class ListTS {
     public void list() {
         for (int c = 0; c < list.length; c++) {
             final int one = 1;
-            for (int i = 0; i < list.length - one; i++) {
-                this.revertPos(i, list[i].hashCode() < list[i + one].hashCode() ? i : i + one);
-            }
+            for (int i = 0; i < list.length - one; i++) this.revertPos(i, list[i].hashCode() < list[i + one].hashCode() ? i : i + one);
         }
     }
 
     public void list(int form) {
         for (int c = 0; c < list.length; c++) {
             final int one = 1;
-            for (int i = 0; i < list.length - one; i++) {
-                this.revertPos(i, list[i].hashCode() < list[i + one].hashCode() ? form == 0 ? i : i + one
-                        : form == 0 ? i + one : i);
-            }
+            for (int i = 0; i < list.length - one; i++) this.revertPos(i, list[i].hashCode() < list[i + one].hashCode() ? form == 0 ? i : i + one : form == 0 ? i + one : i);
         }
     }
 
@@ -71,13 +57,9 @@ public class ListTS {
         return minusOne;
     }
 
-    public int getLength() {
-        return list.length;
-    }
+    public int getLength() { return list.length; }
 
-    public Object getInIndex(int index) {
-        return list[index];
-    }
+    public Object getInIndex(int index) { return list[index]; }
 
     public ListTS find(Object value) { // SLOWER CODE
         ListTS finder = new ListTS();
