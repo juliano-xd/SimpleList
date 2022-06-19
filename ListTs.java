b13b412e8ed386a063aaaa4d45f7d69b52c42bdb
Juliano Xavier Diniz <julianoxavierdinizjuju@gmail.com>
2022-06-19 18:02:03 -0300

    Update

--- ListTs.java
+++ ListTs.java
@@ -17,23 +17,28 @@ public class ListTs{
         final int z = 0;
         if (index < z || index > this.length) return;
         final int one = 1;
+        if(this.length == one && index == z) {clear(); return;}
         Object[] newL = new Object[this.length -= one];
-        for (int i = z; i != this.list.length; i++) newL[i >= index ? i - one : i] = this.list[i];
+        for (int i = z; i != this.list.length; i++) newL[i > index ? i - one : i] = this.list[i];
         this.list = newL;
     }
 
     public void remove(Object object) { remove(getIndexOfObject(object)); }
 
     public void revertPos(int fromHere, int toHere) {
+        final int size = this.length;
         final int z = 0;
-        if (fromHere < z || fromHere > length || toHere < z || toHere > length || fromHere == toHere) return;
-        final Object ob0 = this.list[fromHere];
-        final Object ob1 = this.list[toHere];
-        this.list[fromHere] = ob1;
-        this.list[toHere] = ob0;
+        if (fromHere != toHere && fromHere >= z && toHere >= z && fromHere < size && toHere < size) {
+            final Object ob0 = this.list[fromHere];
+            final Object ob1 = this.list[toHere];
+            this.list[fromHere] = ob1;
+            this.list[toHere] = ob0;
+        }
     }
 
-    public void clear() { this.list = new Object[0]; }
+    public void clear() {this.list = new Object[this.length = 0];}
+
+    public void addAll(ListTs values) { for (Object object : values.list) this.add(object); }
 
     public void list() {
         final int L = this.list.length;
@@ -51,7 +56,13 @@ public class ListTs{
         }
     }
     
-    public void addAll(ListTs values) { for (Object object : values.list) this.add(object); }
+    public void fill(int index, Object value) {
+        for (int i = index; i < list.length; i++) this.list[i] = value;
+    }
+
+    public void replaceAll(Object thisObject, Object toThisObject) {
+        for (int i = 0; i < this.length; i++) if(this.list[i] == thisObject) this.list[i] = toThisObject;
+    }
 
     public int getIndexOfObject(Object value) {
         final Object[] iii = this.list;
@@ -79,8 +90,13 @@ public class ListTs{
     }
 
     public boolean contains(Object value) {
-        for (Object object : this.list) if(value == object) return true;
+        for (Object object : this.list) if(object == value) return true;
         return false;
+    }
+
+    public boolean every(Object value) {
+        for (Object object : this.list) if(object != value) return false;
+        return true;
     }
 
     public String toString() {
